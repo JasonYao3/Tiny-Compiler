0a9501840bedba2cc477b2373ac9e3cef9493e00
Jason Yao <jasonyao105025@gmail.com>
2024-01-14 18:12:11 -0500

    Created tiny_parser class

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from Lexer import *
+from lexer import *
+from tiny_parser import *
+
 
 def main():
-    source = "IF+-123 foo*THEN/"
-    lexer = Lexer(source)
+    print("Tiny Compiler")
 
-    token = lexer.getToken()
-    while token.kind != TokenType.EOF:
-        print(token.kind)
-        token = lexer.getToken()
+    if len(sys.argv) != 2:
+        sys.exit("Error: Compiler needs source file as argument.")
+    with open(sys.argv[1], "r") as inputFile:
+        source = inputFile.read()
+
+    lexer = Lexer(source)
+    parser = Parser(lexer)
+
+    parser.program()
+    print("Parsing completed.")
+
 
 main()
